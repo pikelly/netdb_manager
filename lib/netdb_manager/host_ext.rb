cbfca617b7acc0c83136cee4c4d9afcb7753e710
Paul Kelly <pikelly@blueyonder.co.uk>
2010-10-30 18:21:35 +0100

    Deletion working

--- lib/netdb_manager/host_ext.rb
+++ lib/netdb_manager/host_ext.rb
@@ -20,37 +20,50 @@ module NetdbManager
       base.extend  ClassMethods
       base.send :include, InstanceMethods
       base.class_eval do
-        before_save :check_dns
-        after_save  :update_netdbs
+        before_save   :initialize_proxies, :check_netdbs
+        after_create  :create_netdbs
+        after_update  :update_netdbs
+        after_destroy :initialize_proxies, :destroy_netdbs
       end
       true
     end
 
     module InstanceMethods
-      def check_dns
+      # Checks whether DNS or DHCP entries already exist
+      # Returns: Boolean true if no entries exists
+      def check_netdbs
         continue = true
         @resolver = Resolv::DNS.new :search => domain.name, :nameserver => domain.dns.address
-        if (@resolver.getaddress(name) rescue false)
-          errors.add_to_base "#{name} is already in use"
+        if (address = @resolver.getaddress(name) rescue false)
+          errors.add_to_base "#{name} is already in DNS with an address of #{address}"
           continue = false
         end
-        if (@resolver.getname(ip) rescue false)
-          errors.add_to_base "#{ip} is already in use"
+        if (hostname = @resolver.getname(ip) rescue false)
+          errors.add_to_base "#{ip} is already in the DNS with a name of #{hostname}"
+          continue = false
+        end
+        if (entry = dhcp.get subnet.number, mac) and @dhcp.error.empty?
+          errors.add_to_base "#{subnet}/#{mac} is already managed by DHCP and configures #{entry[:title]}"
           continue = false
         end
         continue
       end
 
-      def save_netdb_caches
-        
-      end
-
+      # Retrieves the DHCP entry for this host via a lookup on the MAC
+      # Returns: Hash  Example {
+      #   "omshell"   :true
+      #   "mac"       :"22:33:44:55:66:11"
+      #   "nextServer":"192.168.122.1"
+      #   "title"     :"brsla804.brs.example.com"
+      #   "filename"  :"pxelinux.0"
+      #   "ip"        :"192.168.122.4"}
       def getDHCP
         log_transaction("Query a DHCP reservation for #{name}/#{ip}", @dhcp){|dhcp|
           dhcp.get subnet.number, mac
         }
       end
 
+      # Deletes the DHCP entry for this host
       def delDHCP
         status = log_transaction("Delete a DHCP reservation for #{name}/#{ip}", @dhcp){|dhcp|
           dhcp.delete subnet.number, mac
@@ -60,18 +73,21 @@ module NetdbManager
           dhcp.delete subnet.number, mac
         }
       end
+
+      def fqdn_puppet
+        @resolver.getaddress(puppetmaster =~ /\./ ? puppetmaster : "#{puppetmaster}.#{domain.name}").to_s
+      rescue Exception => e
+        @dhcp.error = e.message =~/no information for puppet/ ? "Unable to find the address of the puppetmaster in #{domain}" : e.message
+        return false
+      end
+
       # Updates the DHCP scope to add a reservation for this host
       # +returns+ : Boolean true on success
       def setDHCP
         status = log_transaction("Add a DHCP reservation for #{name}/#{ip}", @dhcp){|dhcp|
           #nextserver needs to be an ip
-          #resolver = Resolv::DNS.new :search => domain.name, :nameserver => domain.dns.address
-          begin
-            puppet = @resolver.getaddress(puppetmaster =~ /\./ ? puppetmaster : "#{puppetmaster}.#{domain.name}").to_s
-          rescue Exception => e
-            @dhcp.error = e.message =~/no information for puppet/ ? "Unable to find the address of the puppetmaster in #{domain}" : e.message
-            return false
-          end
+          return false unless puppet = fqdn_puppet
+
           dhcp.set subnet.number, mac,  :name => name, :filename => media.bootfile, :ip => ip, :nextserver => puppet 
         }
         return status unless sp_valid?
@@ -80,6 +96,7 @@ module NetdbManager
         }
       end
 
+      # Returns: String containing the ip in the in-addr.arpa zone
       def to_arpa
         ip.split(/\./).reverse.join(".") + ".in-addr.arpa"
       end
@@ -92,6 +109,16 @@ module NetdbManager
           dns.set(name, :value => to_arpa, :type => "PTR")
         }
       end
+
+      # Removes the host from the forward and backward DNS zones
+      # +returns+ : Boolean true on success
+      def delDNS
+        log_transaction("Delete the DNS records for #{name}/#{ip}", @dns){|dns|
+          dns.delete(name) &&
+          dns.delete(to_arpa)
+        }
+      end
+
       def log_transaction message, server
         logger.info "#{message}"
         unless result = yield(server)
@@ -102,16 +129,46 @@ module NetdbManager
         end
         result
       end
-  
-      def update_netdbs
-        return true if RAILS_ENV == "test"
+
+      def initialize_proxies
         proxy_address = "http://#{subnet.dhcp.address}:4567"
         @dhcp = ProxyAPI::DHCP.new(:url => proxy_address)
         @dns  = ProxyAPI::DNS.new(:url => proxy_address)
+      end
 
+      def destroy_netdbs
+        return true if RAILS_ENV == "test"
+
+        initialize_proxies
+        # We do not care about entries not being present when we delete them but comms errors, etc, must be reported
+        begin
+          if delDHCP
+            unless delDNS
+              if @dns.error !~ /Record/
+                # Rollback the DHCP operation, if you can :-)
+                setDHCP
+                raise RuntimeError, @dns.error
+              end
+            end
+          else
+            if @dhcp.error !~ /Record/
+              raise RuntimeError, @dhcp.error
+            end
+          end
+        rescue  => e
+          errors.add_to_base "Failed to delete the network database entries: " + e.message
+          raise ActiveRecord::Rollback
+          false
+        end
+      end
+
+      def create_netdbs
+        return true if RAILS_ENV == "test"
+
+        initialize_proxies
         Rails.logger.debug "Performing transactional update"
         begin
-          # We have just tested the validity of the DNS operation in check_dns, so if the operation fails it is not due to a conflict
+          # We have just tested the validity of the DNS operation in check_netdbs, so if the operation fails it is not due to a conflict
           # Therefore the operation failed because the write failed and therefore we do not need to rollback the DNS operation
           if setDNS
             unless setDHCP
@@ -122,10 +179,14 @@ module NetdbManager
           end
           true
         rescue => e
-          errors.add_to_base "Failed to update the network databases: " + e.message
+          errors.add_to_base "Failed to create the network database entries: " + e.message
           raise ActiveRecord::Rollback
           false
         end
+      end
+
+      def update_netdbs
+        
       end
 
       def sp_valid?
--- lib/proxy_api.rb
+++ lib/proxy_api.rb
@@ -44,6 +44,10 @@ module ProxyAPI
       self.class.to_s.gsub(/.*::/,"")
     end
 
+    # Decodes the JSON reply if no HTTP error has been detected
+    # If an HTTP error is received then the error messsage is saves into @error
+    # Returns: Response, if the operation is GET, or true for POST, PUT and DELETE.
+    #      OR: false if a HTTP error is detected
     def parse reply
       if reply[0].code == "200"
         if reply[1].size > 2
@@ -57,24 +61,40 @@ module ProxyAPI
       end
     end
 
+    # Perform GET operation on the supplied path
+    # Returns: Array [result, response]
+    #  result  : Object with a code method which is a HTTP error code
+    #  response: HTTP body
     def _get_ path
       @resource[URI.escape(path)].get{|response, request, result| [result, response] }
     rescue Exception => e
       [ OpenStruct.new(:code => "500"), e.message]
     end
 
+    # Perform POST operation with the supplied payload on the supplied path
+    # Returns: Array [result, response]
+    #  result  : Object with a code method which is a HTTP error code
+    #  response: HTTP body
     def _post_ payload, path
       @resource[path].post(payload){|response, request, result| [result, response] }
     rescue Exception => e
       [ OpenStruct.new(:code => "500"), e.message]
     end
 
+    # Perform PUT operation with the supplied payload on the supplied path
+    # Returns: Array [result, response]
+    #  result  : Object with a code method which is a HTTP error code
+    #  response: HTTP body
     def _put_ payload, path
       @resource[path].put(payload){|response, request, result| [result, response] }
     rescue Exception => e
       [ OpenStruct.new(:code => "500"), e.message]
     end
 
+    # Perform DELETE operation on the supplied path
+    # Returns: Array [result, response]
+    #  result  : Object with a code method which is a HTTP error code
+    #  response: HTTP body
     def _delete_ path
       @resource[path].delete{|response, request, result| [result, response] }
     rescue Exception => e
@@ -97,18 +117,34 @@ module ProxyAPI
       super args
     end
 
+    # Retrive the Server's subnets
+    # Returns: Array of Hashes or false
+    # Example [{"network":"192.168.11.0","netmask":"255.255.255.0"},{"network":"192.168.122.0","netmask":"255.255.255.0"}]
     def subnets
       parse(_get_(".json"))
     end
 
+    # Retrieves a DHCP entry
+    # [+subnet+] : String in dotted decimal format
+    # [+mac+]    : String in coloned sexpulet format
+    # Returns    : Hash or false
     def get subnet, mac
       parse(_get_("#{subnet}/#{mac}.json"))
     end
 
+    # Sets a DHCP entry
+    # [+subnet+] : String in dotted decimal format
+    # [+mac+]    : String in coloned sexpulet format
+    # [+args+]   : Hash containing DHCP values. The :mac key is taken from the mac parameter
+    # Returns    : Boolean status
     def set subnet, mac, args
       parse(_post_(args.merge(:mac => mac), "#{subnet}"))
     end
 
+    # Deletes a DHCP entry
+    # [+subnet+] : String in dotted decimal format
+    # [+mac+]    : String in coloned sexpulet format
+    # Returns    : Boolean status
     def delete subnet, mac
       parse(_delete_("#{subnet}/#{mac}"))
     end
@@ -121,10 +157,17 @@ module ProxyAPI
       super args
     end
     
+    # Sets a DNS entry
+    # [+fqdn+] : String containing the FQDN of the host
+    # [+args+] : Hash containing :value and :type: The :fqdn key is taken from the fqdn parameter
+    # Returns  : Boolean status
     def set fqdn, args
       parse(_post_(args.merge(:fqdn => fqdn), ""))
     end
     
+    # Deletes a DNS entry
+    # [+key+] : String containing either a FQDN or a dotted quad plus .in-addr.arpa.
+    # Returns    : Boolean status
     def delete key
       parse(_delete_("#{key}"))
     end
